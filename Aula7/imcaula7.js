a69a47dc5b1c5b68e63b5fe6b535d4492724a6e2
jirayakbc <feliperobertorocha@gmail.com>
2021-08-24 19:48:11 -0300

    Ajustes no código considerando mais variaveis

--- Aula7/imcaula7.js
+++ Aula7/imcaula7.js
@@ -1,10 +1,17 @@
 peso = 110
 altura = 1.62
 
-imc = Math.round(peso/(altura*altura))
-if (imc <= 18)
+imc = (peso/(altura*altura)).toFixed(2)
+console.log(imc)
+if (imc < 18.5)
     console.log("Seu IMC de: " +imc + " vc está abaixo do peso!")
-else if (imc >=30)
-    console.log("Seu IMC de: " +imc + " vc está acima do peso!")
+    else if (imc >=18.5 && imc < 24.9)
+    console.log("Seu IMC de: " +imc + " vc no peso ideal(parabéns)!")
+    else if (imc >=24.9 && imc < 29.9 )
+    console.log("Seu IMC de: " +imc + " vc esta levemente acima do peso")
+    else if (imc >=29.9 && imc < 35.0)
+    console.log("Seu IMC de: " +imc + " vc está com obesidade grau I!")
+    else if (imc >=35.0 && imc < 39.9)
+    console.log("Seu IMC de: " +imc +  "vc está com obesidade grau II ")
 else
-    console.log("Seu IMC de: " +imc + " vc está no peso ideal!")
+    console.log("Seu IMC de: " +imc + " vc está com obesidade grau III")
